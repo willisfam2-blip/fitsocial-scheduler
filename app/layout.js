fbc0700c52f5f3d52bc5f359f22b6dff6fa2f21e
willisfam2-blip <willisfam2@gmail.com>
2026-08-20 19:51:54 -0600

    Add app layout

---
+++ app/layout.js
@@ -0,0 +1,3 @@
+import './globals.css';
+export const metadata={title:'FitSocial Scheduler',description:'Plan and publish Instagram content'};
+export default function RootLayout({children}){return <html lang="en"><body>{children}</body></html>}
